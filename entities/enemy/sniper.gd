feca6d2fc780264c3cf3bb9ffc2c704760aa4c21
Higodoragon <higodoragon@protonmail.com>
2025-03-17 17:07:14 -0300

    cleaned up sniper code, refined laser code

--- entities/enemy/sniper.gd
+++ entities/enemy/sniper.gd
@@ -8,7 +8,7 @@ class_name EnemySniper
 @onready var ai: AIComponent = $AIComponent
 @onready var state: StateMachineComponent = $StateMachineComponent
 @onready var parent: Node3D = get_parent()
-@onready var laser: Laser = $Laser
+@onready var laser_head : Node3D = $LaserHead
 
 @export_group('Sniper-Specific Settings')
 @export_range(1, 100) var damage: int = 5
@@ -21,103 +21,75 @@ class_name EnemySniper
 var sfx_footstep = global.sfx_generic_footsteps
 var last_known_pos: Vector3
 
-# =====
-# STATES
-# =====
-
-# GENERIC
-# -----
-
 var state_pain := [
-	{'delay': stun_time, 'frame': 6},
-	{'goto': state_active}
+	{ delay = stun_time, frame = 6 },
+	{ goto = state_active }
 ]
 
 const state_idle := [
-	{'sticky_call': 'do_searth'},
-	{'delay': 1, 'frame': 0},
-	{'goto': 'state_active'}
+	{ sticky_call = "do_searth" },
+	{ delay = 1, frame = 0 },
+	{ goto = state_active }
 ]
 
-# mandatory
 const state_active := [
-	{sticky_call = "do_active"},
-	{delay = 15, frame = 1},
-	{delay = 15, frame = 2},
-	{loop = true}
+	{ sticky_call = "do_active" },
+	{ delay = 15, frame = 1 },
+	{ delay = 15, frame = 2 },
+	{ loop = true }
 ]
 
-# mandatory
 const state_attack := [
-	{sticky_call = "do_attack_active"},
-	{delay = 20, frame = 3},
-	{delay = 1, frame = 4},
-	{sticky_call = ""},
-	{call = "do_attack"},
-	{delay = 20, frame = 5},
-	{delay = 1, frame = 4},
-	{call = "do_attack"},
-	{delay = 20, frame = 5},
-	{delay = 1, frame = 4},
-	{call = "do_attack"},
-	{delay = 20, frame = 5},
-	{delay = 1, frame = 4},
-	{goto = state_active},
+	{ sticky_call = "" },
+	{ delay = 60, frame = 4 },
+	{ call = "do_attack" },
+	{ delay = 20, frame = 5 },
+	{ delay = 1, frame = 4 },
+	{ goto = state_active },
 ]
-
-# SNIPER SPECIFIC
-# -----
-
-## gtfo away from player
-const state_flee := [
-	{'sticky_call': 'do_flee'},
-	
-]
-
-## track player
-const state_aim := [
-
-]
-
-# =====
-# BUILT-INS
-# =====
 
 func _ready():
 	ai.start_ai()
 
-
 func _physics_process(delta):
 	physics.common_physics(delta)
 
-
-# =====
-#
-# =====
-
-
-func sniper_should_attack():
-	if ai.attack_delay <= 0 and global.check(parent, "state_attack"):
-		parent.state.set_state(parent.state_attack)
-		return true
-
-	if ai.melee_delay <= 0 and ai.in_melee_activation_range() and global.check(parent, "state_melee"):
-		parent.state.set_state(parent.state_melee)
-		return true
+func _process( delta ):
+	if ai.target:
+		var target_position : Vector3 = ai.target.global_position
 		
-	return false
+		if ai.target == global.player:
+			target_position = ai.target.camera.global_position - Vector3( 0, ai.target.view_height, 0 ) - Vector3( 0, view_height, 0 )
 
+		var space_state = get_world_3d().direct_space_state
+		var query = PhysicsRayQueryParameters3D.new()
+		query.collide_with_areas = false
+		query.collide_with_bodies = true
+		query.from = global_position + Vector3( 0, 1, 0 )
+		query.to = ai.target.global_position + Vector3( 0, 1, 0 )
+		query.collision_mask = 1
 
-# =====
-# STATE ACTIONS
-# =====
-
+		var result = space_state.intersect_ray( query )
+		var hit_position := Vector3.ZERO
+		
+		if not result.is_empty():
+			hit_position = result.position - Vector3( 0, 1, 0 )
+		else:
+			hit_position = ai.target.global_position
+		
+		laser_head.position = Vector3.ZERO
+		laser_head.look_at( hit_position )
+		laser_head.scale.z = global_position.distance_to( hit_position )
+		laser_head.position = Vector3( 0, 1, 0 )
+		laser_head.show()
+	else:
+		laser_head.hide()
 
 func do_move():
 	if ai.walk_angle_time <= 0:
 		ai.walk_angle = ai.target_angle()
 		if ai.target_distance() > backup_distance:
-			ai.walk_angle += randf_range(-PI * 0.25, PI * 0.25)
+			ai.walk_angle += randf_range( -PI * 0.25, PI * 0.25 )
 		else:
 			ai.walk_angle += PI
 		ai.walk_angle_time = randi_range(30, 60)
@@ -125,46 +97,16 @@ func do_move():
 	var walk: Vector3 = global.angle_to_direction(ai.walk_angle) * Vector3(1, 0, 1)
 	velocity += walk * speed
 
-
 func do_active():
 	if ai.target:
 		do_move()
 		ai.should_attack()
-		# sniper_should_attack()
-
 
 func do_attack():
-	var attack := Attack.new()
-	attack.agressor = self
-	attack.damage = damage
-	attack.knockback_power = knockback
-	# combat.fire_projectile(self, global_position, ai.target_direction(), projectile_velocity, attack)
-	# for col in res:
-	# 	# print('col: %s' % col)
-	# 	print('col parent: %s' % col.collider.get_parent())
-	combat.hitscan_bullet(self, global_position, ai.target_direction(), attack)
-	ai.attack_delay = 100
-
-
-func do_attack_active():
 	if ai.target:
-		do_move();
-
-
-func do_flee():
-	do_active()
-
-
-func do_aim():
-	pass
-
-
-func _process(delta):
-	if ai.target:
-		update_laser()
-
-
-func update_laser():
-	var res = combat.hitscan(self, global_position, ai.target_direction(), weapon_range, true, true)
-	print(res)
-	laser.update(res[0].collider.get_parent())
+		var attack := Attack.new()
+		attack.agressor = self
+		attack.damage = damage
+		attack.knockback_power = knockback
+		combat.hitscan_bullet( self, global_position, ai.target_direction(), attack )
+		ai.attack_delay = 100
