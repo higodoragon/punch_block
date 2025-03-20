6b41543b2a22e8ae9bd58ed95741eb54c270e246
spacehare <dantath425@gmail.com>
2025-03-20 02:52:02 -0400

    show level author in select button

--- ui/ui_level_preview.gd
+++ ui/ui_level_preview.gd
@@ -1,5 +1,6 @@
 extends Control
 class_name UiLevelPreview
 
-@onready var label: Label = %Label
+@onready var label_title: Label = %LabelTitle
+@onready var label_author: Label = %LabelAuthor
 @onready var button: Button = %Button
