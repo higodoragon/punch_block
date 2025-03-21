4e4657b963e55bda261489c5877c612579f482b2
Higodoragon <higodoragon@protonmail.com>
2025-03-21 16:58:40 -0300

    fixed gib merge problems, sniper is now 2 hp

--- scripts/gib_handler.gd
+++ scripts/gib_handler.gd
@@ -27,13 +27,21 @@ func spawn_gibs(enemy: CharacterBase, killer):
 	# decapitated head
 	var head_inst := phys_sprite_scene.instantiate() as PhysSprite
 	head_inst.assoc = get_correct_assoc(enemy)
-	head_inst.killer_vector = killer_pos
+	#head_inst.killer_vector = killer_pos
 	global.stage.add_child(head_inst)
 	head_inst._sprite.region_enabled = true
 	head_inst.global_position = enemy.global_position
-	head_inst.setup()
+
+	#body_entered.connect(_on_body_entered)
+	head_inst._despawn_timer.timeout.connect( head_inst._despawn )
+
+	head_inst._sprite.texture = head_inst.assoc.texture
+	head_inst._sprite.region_rect = head_inst.assoc.head_rect
+	head_inst.apply_central_impulse( Vector3( randf_range( -4, 4 ), randf_range( 10, 15 ), randf_range( -4, 4 ) ) )
+	head_inst._despawn_timer.start(4.0)	
 
 	# gore gibs; meat
+	var gore_pos = enemy.global_position + Vector3( 0, 1.5, 0 )
 	var gore_amount_min = 0
 	var gore_amount_max := 3 if enemy is not EnemyBrute else 9
 	var rand_gore_amount = randi_range(gore_amount_min, gore_amount_max)
@@ -41,17 +49,18 @@ func spawn_gibs(enemy: CharacterBase, killer):
 		for _i in rand_gore_amount + 1:
 			var gore_inst := phys_sprite_scene.instantiate() as PhysSprite
 			global.stage.add_child(gore_inst)
-			gore_inst.global_position = enemy.global_position
-			gore_inst.global_position.y += 0.5
+			gore_inst.global_position = gore_pos
 			gore_inst._sprite.texture = gore_texture
 			gore_inst._sprite.region_enabled = false
 			gore_inst._sprite.hframes = GORE_FRAMES
 			gore_inst._sprite.frame = randi_range(0, GORE_FRAMES - 1)
-			gore_inst.setup()
+			gore_inst.apply_central_impulse( Vector3( randf_range( -4, 4 ), randf_range( 2, 5 ), randf_range( -4, 4 ) ) )
+			var rand = randf_range(0.1, 10.0)
+			gore_inst.apply_torque( Vector3( rand / 2, rand / 2, rand / 2 ) )
+			gore_inst._despawn_timer.start(2.0)
 
 	# blood explosion
 	var explode_inst := particles_blood_explosion.instantiate() as GPUParticles3D
 	global.stage.add_child(explode_inst)
-	explode_inst.global_position = enemy.global_position
-	explode_inst.global_position.y += 0.5
+	explode_inst.global_position = gore_pos
 	explode_inst.emitting = true
--- scripts/gibber/phys_sprite.gd
+++ scripts/gibber/phys_sprite.gd
@@ -11,27 +11,27 @@ var move_away_from_force: float = 10.0
 const FADE_TIME := 2.0
 var _bounces := 1
 
-func setup():
-	#body_entered.connect(_on_body_entered)
-	_despawn_timer.timeout.connect(_despawn)
-
-	# for heads
-	if assoc:
-		_sprite.texture = assoc.texture
-		# _sprite.position.y -= assoc.y_adjustment
-		_sprite.region_rect = assoc.head_rect
-		global_position.y += assoc.y_adjustment
-		#var force = (global_position - killer_vector).normalized() * move_away_from_force
-		#apply_central_impulse(force)
-		apply_central_impulse( Vector3( randf_range( -4, 4 ), randf_range( 10, 15 ), randf_range( -4, 4 ) ) )
-	# for giblets
-	else:
-		apply_central_impulse( Vector3( randf_range( -4, 4 ), randf_range( 2, 5 ), randf_range( -4, 4 ) ) )
-		#apply_central_impulse((Vector3(randf() - 0.5, randf() - 0.5, randf() - 0.5)) * 20.0)
-
-		#var rand = randf_range(0.1, 10.0)
-		#apply_torque( Vector3( rand / 2, rand / 2, rand / 2 ) )		
-		_despawn_timer.start(4.0)
+#func setup():
+#	#body_entered.connect(_on_body_entered)
+#	_despawn_timer.timeout.connect(_despawn)
+#
+#	# for heads
+#	if assoc:
+#		_sprite.texture = assoc.texture
+#		# _sprite.position.y -= assoc.y_adjustment
+#		_sprite.region_rect = assoc.head_rect
+#		global_position.y += assoc.y_adjustment
+#		#var force = (global_position - killer_vector).normalized() * move_away_from_force
+#		#apply_central_impulse(force)
+#		apply_central_impulse( Vector3( randf_range( -4, 4 ), randf_range( 10, 15 ), randf_range( -4, 4 ) ) )
+#	# for giblets
+#	else:
+#		apply_central_impulse( Vector3( randf_range( -4, 4 ), randf_range( 2, 5 ), randf_range( -4, 4 ) ) )
+#		#apply_central_impulse((Vector3(randf() - 0.5, randf() - 0.5, randf() - 0.5)) * 20.0)
+#
+#		#var rand = randf_range(0.1, 10.0)
+#		#apply_torque( Vector3( rand / 2, rand / 2, rand / 2 ) )		
+#		_despawn_timer.start(4.0)
 
 func _despawn():
 	despawn_tween = get_tree().create_tween()
@@ -40,11 +40,10 @@ func _despawn():
 	despawn_tween.tween_callback(queue_free)
 	
 
-#func _on_body_entered(_body):
-#	if _bounces > 0:
-#		_bounces -= 1
-#		$ParticlesBloodExplosion.emitting = true
-
+func _on_body_entered(_body):
+	if _bounces > 0:
+		_bounces -= 1
+		$ParticlesBloodExplosion.emitting = true
 
 # func _process(_delta):
 # 	var cam := get_viewport().get_camera_3d()
