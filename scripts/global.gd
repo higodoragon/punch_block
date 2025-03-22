6bfcab128bef7cb490949a94fe85db28538f30ed
spacehare <dantath425@gmail.com>
2025-03-22 13:39:11 -0400

    fix map building on export (see desc)
    there were 200+ errors in console every time a map would be built. "data.tree is null". i THINK i fixed this by getting it from an instance ( `.instantiate()` ) and then calling `.add_child()` immediately after that. i am not sure.
    
    additionally, func_godot (or maybe godot itself) was having issues reading the map file on export. godot was secretly converting `res://` paths into `uid://` paths but showing them to me in the inspector as `res://` paths still. so what i had to do was get the ID from the UID, then convert that ID back into the `res://` path that i was being shown in the inspector. and now func_godot loads it properly in the exported build.

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,35 +1,35 @@
 extends Node
 
-@onready var audio : AudioManagerComponent = $AudioManagerComponent
-@onready var stage_container : Node3D = $StageContainer
+@onready var audio: AudioManagerComponent = $AudioManagerComponent
+@onready var stage_container: Node3D = $StageContainer
 @onready var music_handler: MusicHandler = $MusicHandler
 @onready var gib_handler: GibHandler = $GibHandler
 
-var stage : Node3D
-var stage_path : String
+var stage: Node3D
+var stage_path: String
 var stage_textures := "res://func_godot/gamedir/textures/"
 var stage_external := ""
 
-var player : CharacterBase
-var player_position : Vector3 = Vector3.ZERO
-var player_rotation : Vector3 = Vector3.ZERO
-var mouse_mode : int = Input.MOUSE_MODE_CAPTURED
+var player: CharacterBase
+var player_position: Vector3 = Vector3.ZERO
+var player_rotation: Vector3 = Vector3.ZERO
+var mouse_mode: int = Input.MOUSE_MODE_CAPTURED
 @export var mouse_sensitivity: float = 3
 @export var level_order: Array[Level]
 
-var player_active : bool = false
-var pause_active : bool = false:
+var player_active: bool = false
+var pause_active: bool = false:
 	set(val):
 		pause_active = val
 		paused.emit(val)
-var focus_try : bool = false
-var focus_failed : bool = false
+var focus_try: bool = false
+var focus_failed: bool = false
 
-var cheats : bool = false
+var cheats: bool = false
 
-var freezeframe : int = 0
+var freezeframe: int = 0
 
-var enemy_list : Array = []
+var enemy_list: Array = []
 
 signal paused(way: bool)
 
@@ -44,7 +44,7 @@ func _ready():
 	MaterialReferences.compile_material_references()
 	music_handler.play_music(load('res://audio/music/music_no_one_steals.tres'))
 
-func _process( _delta: float ) -> void:
+func _process(_delta: float) -> void:
 	pause_process()
 
 	# HUD DISPLAY
@@ -62,7 +62,7 @@ func _physics_process(delta: float) -> void:
 	if message_time <= 0 and player != null:
 		player.hud_message.text = ""
 
-func _input( event: InputEvent ):
+func _input(event: InputEvent):
 	if focus_failed and event is InputEventMouseButton and event.button_index == 1:
 		focus_try = true
 	
@@ -76,14 +76,14 @@ func _input( event: InputEvent ):
 		pause_active = not pause_active
 		mouse_update()
 	
-	if Input.is_action_just_pressed( "debug_togglefullscreen" ):
+	if Input.is_action_just_pressed("debug_togglefullscreen"):
 		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
-			DisplayServer.window_set_mode( DisplayServer.WINDOW_MODE_FULLSCREEN )
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		else:
-			DisplayServer.window_set_mode( DisplayServer.WINDOW_MODE_WINDOWED )
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		return
 
-	if Input.is_action_just_pressed( "debug_reloadstage" ):
+	if Input.is_action_just_pressed("debug_reloadstage"):
 		reload_stage()
 		return
 	
@@ -91,45 +91,45 @@ func _input( event: InputEvent ):
 		player.do_input_handiling()
 
 func console_defs():
-	Console.add_command("map", console_map, ["map name"] )
-	Console.add_command("reload", reload_stage )
-	Console.add_command("print_targetnames", targetname_print )
-	Console.add_command("escape", on_escape_toggle )
-func console_map( map_name : String ):
+	Console.add_command("map", console_map, ["map name"])
+	Console.add_command("reload", reload_stage)
+	Console.add_command("print_targetnames", targetname_print)
+	Console.add_command("escape", on_escape_toggle)
+func console_map(map_name: String):
 	if map_name == "list":
 		console_map_list()
 		return
 	
 	var map_file := "res://func_godot/maps/%s.map" % map_name
-	if ResourceLoader.exists( map_file ):
-		Console.print_line("changing to map file \"%s\"" % map_file )
-		load_stage( map_file )
+	if ResourceLoader.exists(map_file):
+		Console.print_line("changing to map file \"%s\"" % map_file)
+		load_stage(map_file)
 	else:
-		Console.print_line("map file \"%s\" doesn't exist!" % map_file )
+		Console.print_line("map file \"%s\" doesn't exist!" % map_file)
 
 func console_map_list():
 	const map_dir_path = "res://func_godot/maps"
-	var map_dir := DirAccess.open( map_dir_path )
+	var map_dir := DirAccess.open(map_dir_path)
 	if map_dir != null:
-		Console.print_line( "showing map list from: \"%s\"" % map_dir_path )
+		Console.print_line("showing map list from: \"%s\"" % map_dir_path)
 		
 		map_dir.list_dir_begin()
 		for file: String in map_dir.get_files():
-			if not str( file ).ends_with(".map"):
+			if not str(file).ends_with(".map"):
 				continue
-			Console.print_line( " - %s" % file )
+			Console.print_line(" - %s" % file)
 	else:
-		Console.print_line( "coudn't open map directiory \"%s\"" % map_dir_path )
+		Console.print_line("coudn't open map directiory \"%s\"" % map_dir_path)
 	pass
 
 func process_cmdargs():
 	var raw_args = OS.get_cmdline_user_args()
 	if raw_args.is_empty():
-		print( "no arguments passed!" )
+		print("no arguments passed!")
 		return
 
 	for cmdline_arg in raw_args:
-		print( cmdline_arg )
+		print(cmdline_arg)
 
 		if cmdline_arg == "cheats":
 			cheats = true
@@ -139,24 +139,24 @@ func process_cmdargs():
 		if arg_array.size() != 2:
 			continue
 
-		if cmdline_arg.begins_with( "map::" ):
-			print("start map from: ", arg_array[1] )
+		if cmdline_arg.begins_with("map::"):
+			print("start map from: ", arg_array[1])
 			stage_external = arg_array[1]
 			continue
 				
 		if cmdline_arg.begins_with("map-textures::"):
-			print("textures are being sourced from: ", arg_array[1] )
+			print("textures are being sourced from: ", arg_array[1])
 			stage_textures = arg_array[1]
 			continue
 
 #
 # stage loading
 
-var targets : Dictionary
+var targets: Dictionary
 
 var on_escape = false
 
-func on_escape_check( propeties : Dictionary ):
+func on_escape_check(propeties: Dictionary):
 	if propeties.flags & 1 and on_escape:
 		return true
 
@@ -170,23 +170,23 @@ func on_escape_toggle():
 	on_escape = not on_escape
 
 func targetname_print():
-	print( "printing all targetnames" )
+	print("printing all targetnames")
 	for tn in targets:
-		print( tn )
+		print(tn)
 
-func targetname_add( targetname, node ):
+func targetname_add(targetname, node):
 	if not targetname in targets:
-		targets[ targetname ] = TargetGroup.new()
+		targets[targetname] = TargetGroup.new()
 
-	targets[ targetname ].group.append( node )
+	targets[targetname].group.append(node)
 
-func targetname_activate( targetname, activator ):
-	print( targetname, " is being activated" )
+func targetname_activate(targetname, activator):
+	print(targetname, " is being activated")
 	if not targetname in targets:
-		print( "empty/invalid targetname called" )
+		print("empty/invalid targetname called")
 		return
 		
-	for node in targets[ targetname ].group:
+	for node in targets[targetname].group:
 		if node == null:
 			continue
 
@@ -194,42 +194,42 @@ func targetname_activate( targetname, activator ):
 			continue
 
 		if "do_target_activate" in node:
-			node.do_target_activate( activator )
+			node.do_target_activate(activator)
 
-func funcgodot_common_defs( node : Node, properties : Dictionary ):
-	if not global.on_escape_check( properties ):
+func funcgodot_common_defs(node: Node, properties: Dictionary):
+	if not global.on_escape_check(properties):
 		node.queue_free()
 		return
 
 	if not properties.targetname.is_empty():
-		global.targetname_add( properties.targetname, node )
+		global.targetname_add(properties.targetname, node)
 		node.targetname = properties.targetname
 	
 	if not properties.target.is_empty():
 		node.activate_targetname = properties.target
 	
-	if global.check( node, "message" ):
+	if global.check(node, "message"):
 		node.message = properties.message
 
 var message_time = 0
 
-func message_player( message : String, time : int = 120 ):
-	print( "message to the player: ", message )
+func message_player(message: String, time: int = 120):
+	print("message to the player: ", message)
 	if player:
-		player.hud_message.text = str( "[center]", message, "[/center]" )
+		player.hud_message.text = str("[center]", message, "[/center]")
 
 	message_time = time
 
 func load_next_level():
 	var current_map_file = stage.global_map_file
-	var next_level : Level
+	var next_level: Level
 	for i in level_order.size():
 		var level = level_order[i]
 		if level.map == current_map_file:
 			next_level = level_order[i + 1]
 			break
 	
-	load_level( next_level )
+	load_level(next_level)
 
 func load_level(level: Level):
 	if level.music:
@@ -237,7 +237,7 @@ func load_level(level: Level):
 	global.load_stage(level.map)
 	global.pause_active = false
 
-func load_stage( path : StringName ):
+func load_stage(path: StringName):
 	stage_path = path
 	_load_stage_real.call_deferred()
 
@@ -252,34 +252,44 @@ func _load_stage_real():
 	
 	enemy_list.clear()
 	targets.clear()
+
 	
-	stage = FuncGodotMap.new()
-	stage.global_map_file = stage_path
-	stage.map_settings = preload("res://func_godot/funcgodot_map_settings.tres")
+	stage = preload("res://scripts/fgm.tscn").instantiate()
+	printt(stage, stage_container, stage_path)
+	stage_container.add_child(stage)
+	if 'uid' in stage_path:
+		var res_id = ResourceLoader.get_resource_uid(stage_path)
+		var id_path = ResourceUID.get_id_path(res_id)
+		print('LOADING MAP FROM UID %s' % id_path)
+		
+		stage.local_map_file = id_path
+	else:
+		print('NOT LOADING MAP FROM UID')
+		stage.local_map_file = stage_path
+	# stage.map_settings = preload("res://func_godot/funcgodot_map_settings.tres")
 	stage.map_settings.base_texture_dir = stage_textures
-	print( stage.map_settings.base_texture_dir )
+	print(stage.map_settings.base_texture_dir)
 	stage.verify_and_build()
 	
-	stage_container.add_child( stage )
 	
 	# keep random behavior consistent between levels, restarts and such
-	seed( "A_COOL_SEED".hash() )
+	seed("A_COOL_SEED".hash())
 
 	# add environment
 	var world_environment := WorldEnvironment.new()
-	world_environment.environment = preload( "res://default_environment.tres" )
-	stage.add_child( world_environment )
+	world_environment.environment = preload("res://default_environment.tres")
+	stage.add_child(world_environment)
 	
 	# add player
-	player = preload( "res://entities/info/player.tscn" ).instantiate()
-	stage.add_child( player )
+	player = preload("res://entities/info/player.tscn").instantiate()
+	stage.add_child(player)
 	player.global_position = player_position
 	player.global_rotation = player_rotation
 #
 # pause / focus recover stuff
 
 func mouse_update():
-	set_mouse_mode( get_mouse_sugested_state() )
+	set_mouse_mode(get_mouse_sugested_state())
 
 func get_mouse_sugested_state() -> int:
 	if Console.is_visible():
@@ -294,7 +304,7 @@ func get_mouse_sugested_state() -> int:
 	return Input.MOUSE_MODE_VISIBLE
 
 func pause_pressed():
-	return Input.is_action_just_pressed( "ui_mainmenu" ) or ( OS.get_name() != "Web" and Input.is_action_just_pressed( "ui_cancel" ) )
+	return Input.is_action_just_pressed("ui_mainmenu") or (OS.get_name() != "Web" and Input.is_action_just_pressed("ui_cancel"))
 
 var console_is_visible_old := false
 var console_is_visible := false
@@ -303,7 +313,7 @@ func pause_process():
 	focus_failed = Input.mouse_mode != mouse_mode
 
 	if focus_failed:
-		print( "FOCUS FAILED" )
+		print("FOCUS FAILED")
 
 	if focus_try:
 		if focus_failed:
@@ -319,39 +329,38 @@ func pause_process():
 #
 # random useful funcs
 
-func check( parent : Node, component_name : String ):
-	return ( component_name in parent and component_name != null )
+func check(parent: Node, component_name: String):
+	return (component_name in parent and component_name != null)
 
-func set_mouse_mode( mode : int ):
-	Input.set_mouse_mode( mode )
+func set_mouse_mode(mode: int):
+	Input.set_mouse_mode(mode)
 	mouse_mode = mode
 
-func kill( victim : Node, killer : Node = null ):
-	global.audio_play_at( victim.sfx_death, victim.global_position )
+func kill(victim: Node, killer: Node = null):
+	global.audio_play_at(victim.sfx_death, victim.global_position)
 	
-	if check( victim, "health" ):
+	if check(victim, "health"):
 		victim.health.dead = true
 		if victim.name != 'Player':
 			gib_handler.spawn_gibs(victim, killer)
 
 
-	if check( victim, "activate_targetname" ) and not victim.activate_targetname.is_empty():
-		targetname_activate( victim.activate_targetname, killer )
+	if check(victim, "activate_targetname") and not victim.activate_targetname.is_empty():
+		targetname_activate(victim.activate_targetname, killer)
 	
-	if check( victim, "do_die" ):
-		victim.do_die( killer )
+	if check(victim, "do_die"):
+		victim.do_die(killer)
 	else:
 		victim.queue_free()
 
-func stun( victim : Node, time : int = -1, is_pain : bool = false ):
-
+func stun(victim: Node, time: int = -1, is_pain: bool = false):
 	if victim == null:
 		return
 	
-	if not global.check( victim, "ai" ):
+	if not global.check(victim, "ai"):
 		return
 	
-	if not global.check( victim, "state" ):
+	if not global.check(victim, "state"):
 		return
 
 	if is_pain and victim.pain_time < 0:
@@ -361,9 +370,9 @@ func stun( victim : Node, time : int = -1, is_pain : bool = false ):
 		return
 
 	if is_pain:
-		victim.state.set_state( victim.state_pain )
+		victim.state.set_state(victim.state_pain)
 	else:
-		victim.state.set_state( victim.state_stun )
+		victim.state.set_state(victim.state_stun)
 
 	if time < 0:
 		if is_pain:
@@ -377,24 +386,24 @@ func stun( victim : Node, time : int = -1, is_pain : bool = false ):
 	victim.ai.stun_is_pain = is_pain
 
 
-func rotation_to_direction( global_rotation : Vector3 ) -> Vector3:
-	var direction : Vector3 = Vector3.ZERO
-	direction.x = -sin( global_rotation.y ) * cos( global_rotation.x )	
-	direction.y = sin( global_rotation.x )
-	direction.z = -cos( global_rotation.y ) * cos( global_rotation.x )
+func rotation_to_direction(global_rotation: Vector3) -> Vector3:
+	var direction: Vector3 = Vector3.ZERO
+	direction.x = - sin(global_rotation.y) * cos(global_rotation.x)
+	direction.y = sin(global_rotation.x)
+	direction.z = - cos(global_rotation.y) * cos(global_rotation.x)
 	return direction
 
-func angle_to_direction( angle : float ) -> Vector3:
-	return rotation_to_direction( Vector3( 0, angle, 0 ) )
+func angle_to_direction(angle: float) -> Vector3:
+	return rotation_to_direction(Vector3(0, angle, 0))
 
-func look_at_return( node : Node3D, position : Vector3 ) -> Vector3:
+func look_at_return(node: Node3D, position: Vector3) -> Vector3:
 	var old_rotation := node.rotation
-	node.look_at( position )
+	node.look_at(position)
 	var look_at_target := node.rotation
 	node.rotation = old_rotation
 	return look_at_target
 
-func audio_play_at( sfx : AudioSettings, global_position : Vector3 ):
-	var audio_player = audio.play( sfx )
+func audio_play_at(sfx: AudioSettings, global_position: Vector3):
+	var audio_player = audio.play(sfx)
 	audio_player.global_position = global_position
 	return audio_player
